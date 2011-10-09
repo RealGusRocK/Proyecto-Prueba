6b7f64a7c82d5435b21fa60b9509feebf26afd75
Gustavo <gustavo_ocampo@unitec.edu>
2011-10-09 07:40:53 -0600

    Primer paso del Proyecto

---
+++ src/newpackage/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Main {
+    public static void main(String [] args){
+        System.out.println("Hola Mundo...");
+    }
+    
+}
